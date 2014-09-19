952ac778255c9322cf73158f0482267eee770062
nicolagreco <notsecurity@gmail.com>
2014-09-19 16:15:06 -0700

    non working attempt of colourful output

---
+++ bin/fxos-console.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var path = require('path');
+var Q = require('q');
+var FXOSConsole = require('../');
+var FXOSConnect = require('fxos-connect');
+
+var opts = require("nomnom")
+  .option('manifestURL', {
+    position: 0,
+    help: "App manifest.webapp to deploy",
+    list: false
+  })
+  .option('port', {
+    abbr: 'p',
+    help: 'Port of FirefoxOS'
+  })
+  .option('version', {
+    flag: true,
+    help: 'Print version and exit',
+    callback: function() {
+      fs.readFile(path.resolve(__dirname, '../package.json'), 'utf-8', function(err, file) {
+        console.log(JSON.parse(file).version);
+      });
+    }
+  })
+  .parse();
+
+if (!opts.manifestURL) {
+  opts.manifestURL = path.resolve('./manifest.webapp');
+}
+
+opts.connect = true;
+FXOSConnect(opts, function(err, sim) {
+  if (err) {
+    console.log("Error", err);
+    return;
+  }
+
+  opts.client = sim.client;
+  FXOSConsole(opts, function(err, app) {
+
+
+  })
+
+});
